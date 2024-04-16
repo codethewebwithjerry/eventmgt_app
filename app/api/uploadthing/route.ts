c604f559c0631375a9afffe7a0ec9c9258943efd
Jerry <jeremiahodeniyi@gmail.com>
2024-04-16 13:02:05 +0100

    event update

--- app/api/uploadthing/route.ts
+++ app/api/uploadthing/route.ts
@@ -1,12 +1,3 @@
-//import { createNextRouteHandler } from "uploadthing/next";
- 
-//import { ourFileRouter } from "./core";
- 
-// Export routes for Next App Router
-//export const { GET, POST } = createNextRouteHandler({
-//  router: ourFileRouter,
-//});
-
 import { createRouteHandler } from "uploadthing/next";
  
 import { ourFileRouter } from "./core";
@@ -15,6 +6,5 @@ import { ourFileRouter } from "./core";
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
  
-  // Apply an (optional) custom config:
-  // config: { ... },
+
 });
